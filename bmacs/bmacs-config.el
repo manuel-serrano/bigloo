4e842c5b23f96b6a3bed549ff0b3f8e8eb59fa22
Florian Loitsch <florian.loitsch@sophia.inria.fr>
2009-09-08 15:14:16 +0200

    cross-configuration seems to work again.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,6 +1,6 @@
 ;; Automatically generated file (don't edit) */
-;; Tue Sep  8 09:58:37 CEST 2009
+;; Tue Sep  8 15:11:36 CEST 2009
 
 (provide 'bmacs-config)
-(defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
-(defvar bmacs-lispdir "/users/serrano/emacs/site-lisp/bigloo")
+(defvar bmacs-docdir "/usr/local/doc/bigloo-3.2c")
+(defvar bmacs-lispdir "/home/flo/local/share/emacs/22.3/site-lisp/bigloo")
