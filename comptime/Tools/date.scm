a0a9de06bc34e58b837b031fab9cc4f4acc10e28
Manuel Serrano <Manuel.Serrano@inria.fr>
2009-09-10 13:37:24 +0200

    Full Bootstrap at 10Sep09

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Thu Sep 10 10:28:42 CEST 2009 ")
+(define (bigloo-date)  " Thu Sep 10 13:16:56 CEST 2009 ")
