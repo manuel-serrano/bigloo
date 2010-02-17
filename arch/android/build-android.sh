8e37e5d44bdc6a1b3582d36ee5a3fe2c7df68388
Marcos Dione <mdione@sophia.inria.fr>
2010-02-17 17:24:26 +0100

    ! use eclair's (android 2.0) tools.

--- arch/android/build-android.sh
+++ arch/android/build-android.sh
@@ -1,25 +1,31 @@
 #! /bin/bash
 
-# general public configuration
+set -e
+
+# root for all things android
 ANDROIDROOT=$HOME/src/works/inria/android
 # ANDROIDROOT=/misc/virtual/android
 
-export ANDSRC=$ANDROIDROOT/cupcake-git
+# export ANDSRC=$ANDROIDROOT/cupcake-git
+export ANDSRC=$ANDROIDROOT/eclair-git
 export ANDSDK=$ANDROIDROOT/android-sdk-linux
 
 # adv="test-1.5"
 # try to communicate with the emulator, or fire it if is not there.
 # we can't fire one automatically, so just do it yourself by hand
-export ANDROID_SERIAL="emulator-5554"
+export ANDROID_SERIAL="emulator-5556"
 
 # droid-wrapper
 # http://github.com/tmurakam/droid-wrapper/
 export DROID_ROOT=$ANDSRC
-export DROID_TARGET=generic
+# export DROID_TARGET=generic
+# 3 for cupcake
+# 5 for eclair
+export DROID_TARGET=5
 
 # bigloo/gc
 export CC=$ANDROIDROOT/droid-wrapper/bin/droid-gcc
-export CFLAGS="-DPLATFORM_ANDROID"
+# export CFLAGS="-DPLATFORM_ANDROID" # this should not be needed
 # export CFLAGS="-DPLATFORM_ANDROID -DLINUX"
 
 if [ "$1" == "reconfigure" ]; then
