31e7ab06ca725ca751504e3f24dc18a709095458
Marcos Dione <mdione@sophia.inria.fr>
2010-02-19 17:36:21 +0100

    * s/reconfigure/configure/. * fix bug in script/source detection. * cleanup.

--- arch/android/build-android.sh
+++ arch/android/build-android.sh
@@ -1,34 +1,30 @@
 #! /bin/bash
 
+if [ "$(basename $0)" == "build-android.sh" ]; then
 set -e
+fi
 
 # root for all things android
 ANDROIDROOT=$HOME/src/works/inria/android
 # ANDROIDROOT=/misc/virtual/android
 
-# export ANDSRC=$ANDROIDROOT/cupcake-git
 export ANDSRC=$ANDROIDROOT/eclair-git
 export ANDSDK=$ANDROIDROOT/android-sdk-linux
 
-# adv="test-1.5"
-# try to communicate with the emulator, or fire it if is not there.
-# we can't fire one automatically, so just do it yourself by hand
+# we can't fire a emulator automatically, so just do it yourself by hand
 export ANDROID_SERIAL="emulator-5556"
 
 # droid-wrapper
 # http://github.com/tmurakam/droid-wrapper/
 export DROID_ROOT=$ANDSRC
-# export DROID_TARGET=generic
 # 3 for cupcake
 # 5 for eclair
 export DROID_TARGET=5
 
 # bigloo/gc
 export CC=$ANDROIDROOT/droid-wrapper/bin/droid-gcc
-# export CFLAGS="-DPLATFORM_ANDROID" # this should not be needed
-# export CFLAGS="-DPLATFORM_ANDROID -DLINUX"
 
-if [ "$1" == "reconfigure" ]; then
+if [ "$1" == "configure" ]; then
    ./configure \
       --prefix=$HOME/local/soft/$(basename $(pwd)) \
       --stack-check=no \
