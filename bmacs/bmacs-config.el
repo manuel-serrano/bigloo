689aeb106f481e0c3e10c28a0e60d4b03ad7c3fa
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-03-25 20:45:47 +0100

    bootstrap

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Wed Mar 24 18:53:16 CET 2010
+;; Thu Mar 25 19:56:20 CET 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/home/mdione/local/soft/bigloo-hg-android/doc/bigloo-3.3b")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Mar 24 19:14:44 CET 2010 ")
+(define (bigloo-date)  " Thu Mar 25 20:18:39 CET 2010 ")
