c22de05dad0c54534b3647a8d066e1dc6e9162f4
Marcos Dione <mdione@sophia.inria.fr>
2010-05-04 19:18:52 +0200

    bootstrap

--- comptime/Heap/make.scm
+++ comptime/Heap/make.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Sun Jan  8 08:44:08 1995                          */
-;*    Last change :  Tue Aug 14 10:29:13 2007 (serrano)                */
+;*    Last change :  Thu Apr 15 15:53:34 2010 (serrano)                */
 ;*    Copyright   :  1995-2007 Manuel Serrano, see LICENSE file        */
 ;*    -------------------------------------------------------------    */
 ;*    The creation of a library heap                                   */
@@ -42,7 +42,7 @@
 	     (if (not (binary-port? port))
 		 (error "make-heap" "Can't open output port" hname)
 		 (begin
-		    (output-obj port 
+		    (output-obj port
 				(vector (backend-language (the-backend))
 					*bigloo-version*
 					*bigloo-specific-version*
