7146ae60ab74305ed21798e1ae02c886cb421860
Marcos Dione <mdione@sophia.inria.fr>
2010-06-03 11:20:01 +0200

    * changed name.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,6 +1,6 @@
 ;; Automatically generated file (don't edit) */
-;; Tue May 11 16:42:47 CEST 2010
+;; Thu Jun  3 11:00:37 CEST 2010
 
 (provide 'bmacs-config)
-(defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
-(defvar bmacs-lispdir "/users/serrano/emacs/site-lisp/bigloo")
+(defvar bmacs-docdir "/home/mdione/local/soft/bigloo-hg-android/doc/bigloo-3.3b")
+(defvar bmacs-lispdir "")
