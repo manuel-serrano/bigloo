43834e3b7e53173b93f913e71eb5ad2cb3c07dc1
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-06-18 18:22:57 +0200

    .

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Jun 18 11:39:12 CEST 2010 ")
+(define (bigloo-date)  " Fri Jun 18 11:57:24 CEST 2010 ")
