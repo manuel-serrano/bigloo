eb8094d0280cccf5c448b41c8c3aa5716417fe87
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-06-26 14:24:30 +0200

    Bmem fix.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Tue Jun 22 07:52:04 CEST 2010
+;; Sat Jun 26 14:21:13 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
