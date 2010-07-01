04ddd3035891dd04c182c37ff8e55312fe7da7ef
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-07-01 16:54:25 +0200

    Shipped bootstrap (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Thu Jul  1 14:11:54 CEST 2010
+;; Thu Jul  1 16:47:15 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Thu Jul 1 12:14:45 CEST 2010 ")
+(define (bigloo-date)  " Thu Jul 1 16:50:39 CEST 2010 ")
