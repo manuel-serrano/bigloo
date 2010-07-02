6871e8e3cdf7b342b7396ca09fca2e5e74ffb2f4
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-07-02 10:48:18 +0200

    .

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Fri Jul  2 07:37:00 CEST 2010
+;; Fri Jul  2 10:44:10 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
