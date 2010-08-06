45f8cdb847c016ab790150e89c128443296bb022
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-08-06 08:13:46 +0200

    Fix API makefiles.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Thu Aug  5 14:42:17 CEST 2010
+;; Fri Aug  6 08:06:49 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Thu Aug 5 14:44:49 CEST 2010 ")
+(define (bigloo-date)  " Fri Aug 6 08:09:19 CEST 2010 ")
