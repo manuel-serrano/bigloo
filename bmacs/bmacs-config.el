fb03ac59571df8bfb1b7fe36021bf48a8e132709
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-08-06 18:14:16 +0200

    Fix the documentation.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Fri Aug  6 08:06:49 CEST 2010
+;; Fri Aug  6 18:09:11 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Aug 6 08:09:19 CEST 2010 ")
+(define (bigloo-date)  " Fri Aug 6 18:11:48 CEST 2010 ")
