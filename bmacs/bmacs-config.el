09fecfd379db7e81cf4856a208185dc51ac819e6
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-08-11 15:52:14 +0200

    Installation fixes.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,6 +1,6 @@
 ;; Automatically generated file (don't edit) */
-;; Wed Aug 11 15:00:36 CEST 2010
+;; Wed Aug 11 15:51:30 CEST 2010
 
 (provide 'bmacs-config)
-(defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
-(defvar bmacs-lispdir "/users/serrano/emacs/site-lisp/bigloo")
+(defvar bmacs-docdir "/usr/local/doc/bigloo-3.4b")
+(defvar bmacs-lispdir "/usr/share/emacs/23.2/site-lisp/bigloo")
