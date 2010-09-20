700fdb2a9a2846942ab08d2ee7d965d510177a98
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-09-20 08:12:12 +0200

    Complete maildir implementation (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Tue Sep 14 15:14:14 CEST 2010
+;; Mon Sep 20 08:05:22 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Sep 14 15:18:48 CEST 2010 ")
+(define (bigloo-date)  " Mon Sep 20 08:08:12 CEST 2010 ")
