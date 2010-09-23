f838ca29fe7601b2d2e330a11888ce01966d63d2
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-09-23 15:01:38 +0200

    Various configuration changes for Android.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Wed Sep 22 08:40:57 CEST 2010
+;; Thu Sep 23 13:55:54 CEST 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
