1caa2760a7e0a31557bf007b17a9786acbb3b110
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-10-20 17:00:38 +0200

    Port the new lib linking to Jvm and .NET (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Oct 20 15:53:18 CEST 2010 ")
+(define (bigloo-date)  " Wed Oct 20 16:56:13 CEST 2010 ")
