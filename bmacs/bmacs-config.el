9fcb8a2404184920f90d02efd853fe0eb6658524
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-12-14 08:42:52 +0100

    Script typo fix (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Tue Dec 14 08:07:27 CET 2010
+;; Tue Dec 14 08:35:58 CET 2010
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
