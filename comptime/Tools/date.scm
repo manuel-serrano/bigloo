2e9f1191379aaf43b51f6269650e90adef642883
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-02-18 18:25:34 +0100

    Fix Ubuntu SSL autoconfiguration (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Feb 18 18:14:30 CET 2011 ")
+(define (bigloo-date)  " Fri Feb 18 18:22:02 CET 2011 ")
