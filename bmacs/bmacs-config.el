0e6b8f0673587ee15565c31748e62d761b08d712
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-03-07 10:23:54 +0100

    Accomodate gc7.2alpha5 (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Sat Mar  5 20:56:45 CET 2011
+;; Mon Mar  7 10:17:28 CET 2011
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Sat Mar 5 20:59:25 CET 2011 ")
+(define (bigloo-date)  " Mon Mar 7 10:20:17 CET 2011 ")
