6ff3695220f2f41593f569ea1e77d8b3b369596b
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-05-03 09:10:16 +0200

    Fix vector boxing (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Mon May  2 17:01:03 CEST 2011
+;; Tue May  3 09:02:49 CEST 2011
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
