c18dc76852e17fa709a85afc5fe8ba36c5d0f7da
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-05-13 15:52:40 +0200

    Extend multimedia mpd api (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Fri May 13 12:46:15 CEST 2011
+;; Fri May 13 15:43:51 CEST 2011
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
