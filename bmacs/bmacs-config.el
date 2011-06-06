a567cdf3107743dd9db1f6dbba33cd3c5fe86def
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-06-06 15:45:36 +0200

    SSL Configure fixes (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Mon Jun  6 10:13:13 CEST 2011
+;; Mon Jun  6 15:38:36 CEST 2011
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Jun 6 10:16:54 CEST 2011 ")
+(define (bigloo-date)  " Mon Jun 6 15:41:55 CEST 2011 ")
