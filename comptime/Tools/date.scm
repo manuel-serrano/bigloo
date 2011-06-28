2b38df871c88f44456fc7e34a5441e3a8b8a4414
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-06-28 20:42:10 +0200

    Alsa autoconf fix (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Jun 28 19:59:46 CEST 2011 ")
+(define (bigloo-date)  " Tue Jun 28 20:36:41 CEST 2011 ")
