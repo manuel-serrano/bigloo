35d2014093e0f85e52adc38ced5d67626e4f61ab
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-11-17 06:11:53 +0100

    Class generators elimination (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Thu Nov 17 05:37:24 CET 2011
+;; Thu Nov 17 06:03:29 CET 2011
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
