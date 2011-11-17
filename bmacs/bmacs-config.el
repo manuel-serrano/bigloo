b43f5bbc6f302dc52943d19566a5da3aad48d6b6
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-11-17 08:54:25 +0100

    Create 3.8a branch.

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Thu Nov 17 08:39:55 CET 2011
+;; Thu Nov 17 08:49:40 CET 2011
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
