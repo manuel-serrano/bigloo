b80dbd3d4aba7fbe3298a3d601ee62c407e4a7aa
Manuel Serrano <Manuel.Serrano@inria.fr>
2012-01-06 09:57:25 +0100

    Cleanup object serialization (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Thu Dec 22 18:29:37 CET 2011
+;; Fri Jan  6 09:51:17 CET 2012
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Thu Dec 22 18:33:04 CET 2011 ")
+(define (bigloo-date)  " Fri Jan 6 09:54:46 CET 2012 ")
