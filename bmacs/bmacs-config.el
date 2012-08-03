6e485e1f557b123b736c70cc22834ff4087e2b2a
Manuel Serrano <Manuel.Serrano@inria.fr>
2012-08-03 18:35:44 +0200

    . (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Tue Jul 31 06:30:01 CEST 2012
+;; Fri Aug  3 18:27:59 CEST 2012
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Jul 31 06:34:09 CEST 2012 ")
+(define (bigloo-date)  " Fri Aug 3 18:32:41 CEST 2012 ")
