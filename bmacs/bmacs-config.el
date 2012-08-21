888e79fceae6abf93be473f2aa68077bb4461edd
Manuel Serrano <Manuel.Serrano@inria.fr>
2012-08-21 15:55:59 +0200

    Switch to gc-7.2d (bootstrap)

--- bmacs/bmacs-config.el
+++ bmacs/bmacs-config.el
@@ -1,5 +1,5 @@
 ;; Automatically generated file (don't edit) */
-;; Thu Aug 16 11:24:21 CEST 2012
+;; Tue Aug 21 15:48:38 CEST 2012
 
 (provide 'bmacs-config)
 (defvar bmacs-docdir "/users/serrano/prgm/project/bigloo/manuals")
