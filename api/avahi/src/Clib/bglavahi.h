aef6130b9eacffe8cd004f6942d6974a91641d0f
Manuel Serrano <Manuel.Serrano@inria.fr>
2012-08-28 08:07:33 +0200

    .

--- api/avahi/src/Clib/bglavahi.h
+++ api/avahi/src/Clib/bglavahi.h
@@ -1,7 +1,7 @@
 /*===========================================================================*/
 /*   (Llib/avahi.scm)                                                        */
 /*   Bigloo (3.8d)                                                           */
-/*   Inria -- Sophia Antipolis (c)       Mon Aug 27 16:35:02 CEST 2012       */
+/*   Inria -- Sophia Antipolis (c)       Tue Aug 28 07:57:56 CEST 2012       */
 /*===========================================================================*/
 /* COMPILATION: (/users/serrano/prgm/project/bigloo/bin/bigloo -O3 -fcfa-arithmetic -q -I Llib -lib-dir /users/serrano/prgm/project/bigloo/lib/3.8d -unsafe -safee -srfi avahi -copt -D_REENTRANT -copt -fPIC -copt -IClib -copt  Llib/avahi.scm -o Clib/bglavahi.h -hgen)*/
 
