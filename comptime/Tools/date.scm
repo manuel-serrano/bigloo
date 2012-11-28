e420882fd2dfe843f736f6600488130c6e153e93
Manuel Serrano <Manuel.Serrano@inria.fr>
2012-11-28 11:37:24 +0100

    Remove useless output port specific mutex (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Nov 28 11:18:10 CET 2012 ")
+(define (bigloo-date)  " Wed Nov 28 11:33:11 CET 2012 ")
