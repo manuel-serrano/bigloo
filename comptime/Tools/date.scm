f877d5cb726605f032ddc35d474fdf4eadf75250
Manuel Serrano <Manuel.Serrano@inria.fr>
2013-01-09 10:34:36 +0100

    Accomodate mutex backend info (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Jan 9 10:17:43 CET 2013 ")
+(define (bigloo-date)  " Wed Jan 9 10:31:30 CET 2013 ")
