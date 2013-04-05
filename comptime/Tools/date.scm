923d4309fabc3e2fb23fbea945721c59ae921812
Manuel Serrano <Manuel.Serrano@inria.fr>
2013-04-05 14:53:35 +0200

    Datagram socket input (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Apr 5 11:13:23 CEST 2013 ")
+(define (bigloo-date)  " Fri Apr 5 14:50:04 CEST 2013 ")
