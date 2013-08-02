f73ce8bc82320da6df8a332f288ee85a9a39c039
Manuel Serrano <Manuel.Serrano@inria.fr>
2013-08-02 07:53:28 +0200

    Eval, tail-rec expansion of multiple-value-bind

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Aug 2 07:42:09 CEST 2013 ")
+(define (bigloo-date)  " Fri Aug 2 07:51:18 CEST 2013 ")
