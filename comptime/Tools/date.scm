a79e6b8f3e755718ba22e87194423808f8716395
Manuel Serrano <Manuel.Serrano@inria.fr>
2013-11-27 19:03:54 +0100

    New configure option for pcre (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Nov 25 13:52:16 CET 2013 ")
+(define (bigloo-date)  " Wed Nov 27 19:02:05 CET 2013 ")
