cfbdd794b984199054dbf79a966fb029348e1072
Manuel Serrano <Manuel.Serrano@inria.fr>
2013-12-02 14:39:18 +0100

    Fix srf1 compilation (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Dec 2 13:51:15 CET 2013 ")
+(define (bigloo-date)  " Mon Dec 2 14:37:04 CET 2013 ")
