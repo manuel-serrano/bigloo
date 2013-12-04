74a4df299df69e0ebfee9bcbd9546292f72845f8
Manuel Serrano <Manuel.Serrano@inria.fr>
2013-12-04 08:27:47 +0100

    Switch to gc-7.4.0 (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Dec 3 15:18:32 CET 2013 ")
+(define (bigloo-date)  " Wed Dec 4 08:25:57 CET 2013 ")
