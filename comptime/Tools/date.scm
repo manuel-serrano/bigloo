d51725d5db90a195428d2f6cfc24b82f32ec2f24
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-02-15 10:18:45 +0100

    Fix GC configuration (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Feb 12 10:06:06 CET 2014 ")
+(define (bigloo-date)  " Sat Feb 15 10:16:57 CET 2014 ")
