507fb72c47d71aff4b3ab95ade0448229396cc1f
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-02-18 19:21:15 +0100

    4.1a final (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Feb 18 16:10:56 CET 2014 ")
+(define (bigloo-date)  " Tue Feb 18 19:19:29 CET 2014 ")
