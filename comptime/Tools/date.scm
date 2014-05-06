542c2b626758531113f99c4ec1a40354b3ca0df7
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-05-06 15:18:44 +0200

    Libuv binding (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Apr 28 08:17:33 CEST 2014 ")
+(define (bigloo-date)  " Tue May 6 15:16:28 CEST 2014 ")
