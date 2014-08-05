f5d959824a45825f64e9279dad4b9dc5368ebe4b
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-08-05 06:56:24 +0200

    libuv additions (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Aug 4 08:04:29 CEST 2014 ")
+(define (bigloo-date)  " Tue Aug 5 06:54:19 CEST 2014 ")
