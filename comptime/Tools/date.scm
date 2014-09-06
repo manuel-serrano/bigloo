ce58ce06b2866ea32ecb055fb4cf8b0a665420d6
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-09-06 16:03:53 +0200

    input-string-port extensions (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Sat Sep 6 09:46:01 CEST 2014 ")
+(define (bigloo-date)  " Sat Sep 6 16:01:51 CEST 2014 ")
