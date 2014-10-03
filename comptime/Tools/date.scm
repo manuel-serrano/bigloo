52b99dffedc704fbb0ebfb7ff4bfdfb0afd5ce61
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-10-03 18:26:37 +0200

    Minor JVM bug fix (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Oct 3 18:16:18 CEST 2014 ")
+(define (bigloo-date)  " Fri Oct 3 18:24:28 CEST 2014 ")
