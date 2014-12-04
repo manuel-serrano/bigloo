b402425a79b568bebb87e57ead9b07b48d41a689
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-12-04 07:53:59 +0100

    . (bootstrap)

--- api/avahi/src/Clib/bglavahi.h
+++ api/avahi/src/Clib/bglavahi.h
@@ -1,7 +1,7 @@
 /*===========================================================================*/
 /*   (Llib/avahi.scm)                                                        */
 /*   Bigloo (4.2a)                                                           */
-/*   Inria -- Sophia Antipolis (c)       Sun Nov 30 09:51:10 CET 2014        */
+/*   Inria -- Sophia Antipolis (c)       Thu Dec 4 07:51:54 CET 2014         */
 /*===========================================================================*/
 /* COMPILATION: (/home/serrano/prgm/project/bigloo/bin/bigloo -O3 -fcfa-arithmetic -q -I Llib -lib-dir /home/serrano/prgm/project/bigloo/lib/bigloo/4.2a -unsafe -safee -srfi avahi -copt -D_REENTRANT -copt -fPIC -copt -IClib -copt  Llib/avahi.scm -o Clib/bglavahi.h -hgen)*/
 
--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Sun Nov 30 09:51:10 CET 2014 ")
+(define (bigloo-date)  " Thu Dec 4 07:51:54 CET 2014 ")
