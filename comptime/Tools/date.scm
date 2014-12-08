ed8d71f845674f55d960705c9cccdd74b08fc6b0
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-12-08 11:35:38 +0100

    Intext changes (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Dec 8 11:07:50 CET 2014 ")
+(define (bigloo-date)  " Mon Dec 8 11:33:32 CET 2014 ")
