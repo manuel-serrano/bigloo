990edc1d3a3c34bb2e3ef19cb74a09c5a2336e76
Manuel Serrano <Manuel.Serrano@inria.fr>
2015-04-08 11:09:54 +0200

    multipart cgi parsing (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Apr 8 10:59:17 CEST 2015 ")
+(define (bigloo-date)  " Wed Apr 8 11:07:39 CEST 2015 ")
