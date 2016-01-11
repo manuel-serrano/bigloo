1330d655cd2dc9920445d95e5daf62b4a3b6e3b1
Manuel Serrano <Manuel.Serrano@inria.fr>
2016-01-11 10:25:04 +0100

    Fix random 0 (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Mon Jan 4 07:50:04 CET 2016 ")
+(define (bigloo-date)  " Mon Jan 11 10:22:33 CET 2016 ")
