b6b939ae89c1a184fb6eb33bc146403704392142
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-06-22 08:58:28 +0200

    Autoconf and install (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Jun 20 21:27:00 CEST 2017 ")
+(define (bigloo-date)  " Thu Jun 22 08:56:25 CEST 2017 ")
