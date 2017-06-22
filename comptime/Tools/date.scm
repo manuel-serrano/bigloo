50d3a74727b56b107cb7204974a80efe5c7b090f
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-06-22 09:36:47 +0200

    distrib fix (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Thu Jun 22 09:18:36 CEST 2017 ")
+(define (bigloo-date)  " Thu Jun 22 09:34:44 CEST 2017 ")
