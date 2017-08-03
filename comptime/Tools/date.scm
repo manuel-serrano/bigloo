a40bb4239fa4218e6e1f2725c52858b959d439af
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-08-03 12:12:11 +0200

    Beginnnig of the new object tagging scheme (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Thu Aug 3 09:09:18 CEST 2017 ")
+(define (bigloo-date)  " Thu Aug 3 12:10:04 CEST 2017 ")
