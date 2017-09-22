90bc6c36867381d3da50fa80009bc99a7817c1b2
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-09-22 08:53:49 +0200

    Fix configure (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Sep 22 08:36:13 CEST 2017 ")
+(define (bigloo-date)  " Fri Sep 22 08:51:41 CEST 2017 ")
