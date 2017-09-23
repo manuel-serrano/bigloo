71677335f83917123db14917aeb8751f6e37ec60
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-09-23 09:51:22 +0200

    Improved cycle read performance (bootstrap)

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Fri Sep 22 08:51:41 CEST 2017 ")
+(define (bigloo-date)  " Sat Sep 23 09:49:15 CEST 2017 ")
