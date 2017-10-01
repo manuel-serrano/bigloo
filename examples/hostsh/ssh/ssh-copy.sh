2057df1cbee9afee3acb7761cf9a8c56ae372b67
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-10-01 10:03:54 +0200

    Cross compilation configuration (bootstrap)

--- examples/hostsh/ssh/ssh-copy.sh
+++ examples/hostsh/ssh/ssh-copy.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-TARGET=$1; shift;
+
+REMOTE_HOST=localhost
+REMOTE_SSH_PORT=2022
+REMOTE_USER=hop
+
 FILE=$1;
+
 if [ -f $FILE ]; then
-  EXEC=/tmp/`basename $FILE`
-  scp $FILE $TARGET:$EXEC
-  ssh $TARGET "chmod +x $EXEC"
+  exec=/tmp/`basename $FILE`
+  scp -P $REMOTE_SSH_PORT $FILE $REMOTE_USER@$REMOTE_HOST:$exec
+  ssh -p $REMOTE_SSH_PORT $REMOTE_USER@$REMOTE_HOST "chmod +x $exec"
 else
-  EXEC=$*
+  exec=$*
 fi
-ssh $TARGET "$EXEC"
-RES=$?
-ssh $TARGET "rm $EXEC"
-exit $RES
+ssh -p $REMOTE_SSH_PORT $REMOTE_USER@$REMOTE_HOST "$exec"
+res=$?
+ssh -p $REMOTE_SSH_PORT $REMOTE_USER@$REMOTE_HOST "rm $exec"
+exit $res
