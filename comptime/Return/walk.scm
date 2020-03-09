72330fb76f0549cb4f5fb7000026d534643d3052
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-03-09 07:46:58 +0100

    .

--- comptime/Return/walk.scm
+++ comptime/Return/walk.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Tue Sep  7 05:11:17 2010                          */
-;*    Last change :  Sun Mar  8 18:05:44 2020 (serrano)                */
+;*    Last change :  Mon Mar  9 07:46:48 2020 (serrano)                */
 ;*    Copyright   :  2010-20 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Replace set-exit/unwind-until with return. Currently this pass   */
@@ -113,7 +113,9 @@
 ;*    	          (let ((res1015 EXITNODE))           ;; step5         */
 ;*    	             (begin                           ;; step6         */
 ;*                      (pop-exit!)                   ;; step7         */
-;*                      (f res1015)))))))             ;; step8         */
+;*                      (f res1015)))))))             ;; step8 (*)     */
+;*                                                                     */
+;*    (*) the expression might also be (f (cast res1015 ...))          */
 ;*                                                                     */
 ;*    It returns <EXITVAR x EXITNODE> or #f                            */
 ;*---------------------------------------------------------------------*/
