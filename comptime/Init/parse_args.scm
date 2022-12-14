21de8cbc31e5f30550acb7f4f728a40c784b0d5f
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-12-14 13:03:29 +0100

    Merge

--- comptime/Init/parse_args.scm
+++ comptime/Init/parse_args.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Sun Aug  7 11:47:46 1994                          */
-;*    Last change :  Thu Nov  3 11:06:06 2022 (serrano)                */
+;*    Last change :  Wed Dec 14 12:45:06 2022 (serrano)                */
 ;*    Copyright   :  1992-2022 Manuel Serrano, see LICENSE file        */
 ;*    -------------------------------------------------------------    */
 ;*    The command line arguments parsing                               */
@@ -169,6 +169,7 @@
 					   (os-tmp)))
 		      '(("BIGLOOLIB" . "libraries' directory")
 			("BIGLOOHEAP" . "the initial heap size in megabytes (4 MB by default)")
+			("BIGLOOMAXHEAP" . "the maxium heap size in megabytes (unlimited by default)")
 			("BIGLOOSTACKDEPTH" . "the error stack depth printing")
 			("BIGLOOLIVEPROCESS" . "the maximum number of Bigloo live processes")
 			("BIGLOOTRACE" . "list of active traces"))))
