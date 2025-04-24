1b8cf04d27f7d568019184e6443a306656cf7442
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-04-24 18:33:49 +0200

    Wasm fix and jvm symbol improvement

--- runtime/Jlib/symbol.java
+++ runtime/Jlib/symbol.java
@@ -10,7 +10,7 @@ public class symbol extends obj
       public boolean marked = false;
       public bucket next;
 
-      public bucket( final symbol symb, final bucket next ) {
+      public bucket(final symbol symb, final bucket next) {
 	 this.symb = symb;
 	 this.next = next;
       }
@@ -19,54 +19,77 @@ public class symbol extends obj
    public final byte[] string;
    public Object cval;
 
-   public static final bucket[] table = new bucket[4096];
+   public static int shift = 12;
+   public static bucket[] table = new bucket[1 << shift];
+   public static int cnt = 0;
 
-   public symbol( final Object cval, final byte[] string ) {
+   public symbol(final Object cval, final byte[] string) {
       this.cval = cval;
       this.string = string;
    }
 
-   public static boolean exists( final byte[] name ) {
-      synchronized( table ) {
-	 final int hash_number = foreign.get_hash_power_number( name, 12 );
+   public static boolean exists(final byte[] name) {
+      synchronized(table) {
+	 final int hash_number = foreign.get_hash_power_number(name, shift);
 	 final bucket bucket = table[hash_number];
 
 	 if (bucket == null)
 	    return false;
 
 	 for (bucket run = bucket; (run != null); run = run.next)
-	    if (foreign.bigloo_strcmp( run.symb.string, name ))
+	    if (foreign.bigloo_strcmp(run.symb.string, name))
 	       return true;
 
 	 return false;
       }
    }
 
-   public static symbol make_symbol( final byte[] name ) {
-      synchronized( table ) {
-	 final int hash_number = foreign.get_hash_power_number( name, 12 );
+   public static bucket[] resize_table(bucket[] old_table, int new_shift) {
+      bucket[] new_table = new bucket[1 << new_shift];
+      int old_size = old_table.length;
+      int i;
+
+      for (i = 0; i < old_size; i++) {
+	 for (bucket run = table[i]; (run != null) ; run = run.next) {
+	    symbol symb = run.symb;
+	    int hash = foreign.get_hash_power_number(symb.string, new_shift);
+	    table[hash] = new bucket(symb, table[hash]);
+	 }
+      }
+      
+      return new_table;
+   }
+   
+   public static symbol make_symbol(final byte[] name) {
+      synchronized(table) {
+	 final int hash_number = foreign.get_hash_power_number(name, shift);
 	 final bucket bucket = table[hash_number];
 
 	 if (bucket == null) {
-	    final symbol symbol = new symbol( nil.nil, name );
+	    final symbol symbol = new symbol(nil.nil, name);
 
-	    table[hash_number] = new bucket( symbol, null );
+	    table[hash_number] = new bucket(symbol, null);
 	    return symbol;
 	 }
 
 	 for (bucket run = bucket; (run != null) ; run = run.next)
-	    if (foreign.bigloo_strcmp( run.symb.string, name ))
+	    if (foreign.bigloo_strcmp(run.symb.string, name))
 	       return run.symb;
 
-	 final symbol result = new symbol( nil.nil, name );
-
-	 table[hash_number] = new bucket( result, bucket );
-
-	 return result;
+	 if (cnt++ > (1 << shift) * 4) {
+	    shift++;
+	    table = resize_table(table, shift);
+	 } else {
+	    final symbol result = new symbol(nil.nil, name);
+	    table[hash_number] = new bucket(result, bucket);
+	    return result;
+	 }
       }
+      // this point is reached when the table has been resized
+      return make_symbol(name);
    }
 
-   public void write( final output_port p ) {
-      p.write( string );
+   public void write(final output_port p) {
+      p.write(string);
    }
 }
