6da0fd735b0bb396d57f30f8b6efc207ee6d5574
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-07-15 08:42:40 +0200

    more typo

--- runtime/Clib/inline_alloc_thread.h
+++ runtime/Clib/inline_alloc_thread.h
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Thu Oct 26 15:50:11 2017                          */
-/*    Last change :  Mon Jun 30 08:06:50 2025 (serrano)                */
+/*    Last change :  Tue Jul 15 08:42:26 2025 (serrano)                */
 /*    Copyright   :  2017-25 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    Multi-threaded Boehm allocations                                 */
@@ -91,10 +91,29 @@ make_cell(obj_t val) {
 #ifndef BGL_MAKE_REAL
 #define BGL_MAKE_REAL
 
-#if (!defined(TAG_REALZ))
-BGL_DEFINE_REAL(bgl_zero, bgl_zero_tmp, 0.);
-BGL_DEFINE_REAL(bgl_negative_zero, bgl_negative_zero_tmp, -0.);
-#endif
+#if (!BGL_NAN_TAGGING && !BGL_NUN_TAGGING) 
+#  if (!defined(TAG_REALZ))
+#    if (!defined(TAG_REAL))
+#      define BGL_CREATE_SLOW_REAL(aux, flonum) \
+         static struct { __CNST_ALIGN header_t header; double val; } \
+            aux = { __CNST_FILLER BGL_MAKE_HEADER(REAL_TYPE, 0), flonum }
+#      define BGL_DECLARE_SLOW_REAL(n, aux) \
+         static obj_t n = BREAL(&(aux.header))
+#    else
+#      define BGL_CREATE_SLOW_REAL(aux, flonum) \
+         static struct { double val; } \
+           aux = { flonum }
+#      define BGL_DECLARE_SLOW_REAL(n, aux) \
+         static obj_t n = BREAL(&(aux.val))
+#    endif
+
+#    define BGL_DEFINE_SLOW_REAL(name, aux, flonum) \
+       BGL_CREATE_SLOW_REAL(aux, flonum); \
+       BGL_DECLARE_SLOW_REAL(name, aux)
+
+BGL_DEFINE_SLOW_REAL(bgl_zero, bgl_zero_tmp, 0.);
+BGL_DEFINE_SLOW_REAL(bgl_negative_zero, bgl_negative_zero_tmp, -0.);
+#  endif
 
 #if (!BGL_NAN_TAGGING && !BGL_NUN_TAGGING) 
 GC_API obj_t
