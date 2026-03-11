554698f3c5fe1f27541664d3049b2940861d0cc4
Manuel Serrano <Manuel.Serrano@inria.fr>
2026-03-11 13:50:08 +0100

    mpg123 lib5

---
+++ api/mpg123/src/Misc/make_lib5.scm
@@ -0,0 +1,21 @@
+;*=====================================================================*/
+;*    .../project/bigloo/5.0a/api/mpg123/src/Misc/make_lib5.scm        */
+;*    -------------------------------------------------------------    */
+;*    Author      :  Manuel Serrano                                    */
+;*    Creation    :  Tue Nov  6 15:09:37 2001                          */
+;*    Last change :  Wed Mar 11 13:49:34 2026 (serrano)                */
+;*    Copyright   :  2001-26 Manuel Serrano                            */
+;*    -------------------------------------------------------------    */
+;*    The module used to build the heap file.                          */
+;*=====================================================================*/
+
+;*---------------------------------------------------------------------*/
+;*    The module                                                       */
+;*---------------------------------------------------------------------*/
+(module mpg123
+   (export :version 4 "../Llib/mpg123.scm")
+   (export :version 4 "../Llib/mpg123_decoder.scm")
+   (export :version 4 "../Llib/mpg123_alsadec.scm")
+   (export :version 4 "../Llib/mpg123_padec.scm"))
+
+
